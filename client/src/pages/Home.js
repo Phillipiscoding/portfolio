6213a842fa692a8ced58a33ceffb850e2bc81298
PhillipHarden <phillipiscoding@gmail.com>
2023-07-04 22:14:26 -0400

    cursor added along with other animations and updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Hero from "../images/phillip.png";
+import BouncingText from "../components/BouncingText";
 
 
 function Home() {
+  const greeting = "Hi, I'm Phillip.".split("");
   return (
     <div className="home container">
       
       <div>
-        <h1 className="brand-font">Hi, I'm Phillip.</h1>
-        <p>I am a <span>Web Developer</span> specializing in Frontend development, dedicated to creating engaging and user-friendly Websites and Web Applications. By crafting exceptional Frontend experiences</p>
+        <h1 className="brand-font">{greeting.map((letter, index) =>{
+          return <BouncingText key={index}>
+            {letter}
+          </BouncingText>
+        })}</h1>
+        <p>I am a <span className="webdev">Web Developer</span> specializing in Frontend development, dedicated to creating engaging and user-friendly Websites and Web Applications. By crafting exceptional Frontend experiences</p>
       </div>  
       <div>
         <img src={Hero} />
