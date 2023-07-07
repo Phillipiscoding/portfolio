9e9af4562aae70ce17a36aa56a611ee294e7ecdf
PhillipHarden <phillipiscoding@gmail.com>
2023-07-07 03:01:14 -0400

    working on validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,63 @@
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { useNavigate } from "react-router-dom";
 
 function ContactForm() {
+  const [input, setInput] = useState({
+    "from_name": "",
+    "from_email": "",
+    "message": "",
+  });
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
+
   const form = useRef();
   const serviceId = "service_0h9zksq";
   const templateId = "template_6ahtpil";
   const publicKey = "IT_6Gd78lg1g9xJG0";
 
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
+    console.log(input);
+    if (input.from_name == "") {
+      console.log("Please enter your name.");
+      setError("Please enter your name.");
+    }
+    if (input.from_email == "") {
+      console.log("Please enter your email address.");
+      setError("Please enter your email address.");
+    }
+    if (input.message == "") {
+      console.log("Please enter a message.");
+      setError("Please enter a message.");
+    }
+    if (err == null && input.from_name == "" && input.from_email == "" &&input.message == "") {
+      console.log("It's good to go");
+    }
+
+    // emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+    //   (result) => {
+    //     console.log(result.text); // respondes with "OK" if accpeted and sent
+    //   },
+    //   (error) => {
+    //     console.log(error.text);
+    //   }
+    // );
   };
+
+  const acceptErrorMessage = () => {
+    console.log("ok error");
+    setError(null);
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInput((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  console.log(input);
 
   return (
     <form className="contact-form" ref={form} onSubmit={sendEmail}>
@@ -28,7 +67,7 @@ function ContactForm() {
           placeholder="Enter your name"
           type="text"
           name="from_name"
-          required
+          onChange={handleChange}
         />
       </div>
       <div className="input-container">
@@ -37,7 +76,7 @@ function ContactForm() {
           placeholder="Enter your email"
           type="email"
           name="from_email"
-          required
+          onChange={handleChange}
         />
       </div>
       <div className="input-container">
@@ -46,12 +85,28 @@ function ContactForm() {
           placeholder="Enter your message"
           name="message"
           rows="5"
-          required
+          onChange={handleChange}
         ></textarea>
       </div>
       <div>
         <input className="btn btn-primary" type="submit" value="Submit" />
       </div>
+
+      {err && (
+        <div className="error-message">
+          <h4>Error</h4>
+          <div className="error-message-container">
+            <p>{err}</p>
+          </div>
+          <button
+            className="btn btn-primary error-btn"
+            type="button"
+            onClick={acceptErrorMessage}
+          >
+            OK
+          </button>
+        </div>
+      )}
     </form>
   );
 }
